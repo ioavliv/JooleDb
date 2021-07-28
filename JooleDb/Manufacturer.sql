65ca8dfe7ed50e3c95ba86995db0d6e22eacaed7
GWest96 <76221856+GWest96@users.noreply.github.com>
2021-07-28 14:16:18 -0400

    Update Manufacturer.sql

--- JooleDb/Manufacturer.sql
+++ JooleDb/Manufacturer.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Manufacturer]
 (
 Manufacturer_ID int NOT NULL PRIMARY KEY,
-U_ID int FOREIGN KEY REFERENCES [User](User_ID) NULL,
+U_ID int FOREIGN KEY REFERENCES User(User_ID) NULL,
 Name varchar(50) NOT NULL
 );
