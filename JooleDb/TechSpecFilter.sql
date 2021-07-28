44120ed6091c43e8de1a451bcce460a27663ad1e
GWest96 <76221856+GWest96@users.noreply.github.com>
2021-07-28 14:35:06 -0400

    Update TechSpecFilter.sql

--- JooleDb/TechSpecFilter.sql
+++ JooleDb/TechSpecFilter.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[TechSpecFilter]
 (
-Prop_ID int FOREIGN KEY REFERENCES Property(Property_ID) NOT NULL,
-SC_ID int FOREIGN KEY REFERENCES SubCategory(SubCategory_ID) NOT NULL,
+Property_ID int FOREIGN KEY REFERENCES Property(Property_ID) NOT NULL,
+SubCategory_ID int FOREIGN KEY REFERENCES SubCategory(SubCategory_ID) NOT NULL,
 Max_Value float,
 Min_Value float
 Primary Key (Property_ID,Subcategory_ID)
