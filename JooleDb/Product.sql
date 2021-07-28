0103d708a7887d6b5c67654c5ae10ee5ce9310b4
GWest96 <76221856+GWest96@users.noreply.github.com>
2021-07-28 14:34:08 -0400

    Update Product.sql

--- JooleDb/Product.sql
+++ JooleDb/Product.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Product]
 (
 Product_ID int NOT NULL PRIMARY KEY,
-SC_ID int FOREIGN KEY REFERENCES SubCategory(SubCategory_ID) NULL,
-M_ID int FOREIGN KEY REFERENCES Manufacturer(Manufacturer_ID) NULL,
+Subcategory_ID int FOREIGN KEY REFERENCES SubCategory(SubCategory_ID) NULL,
+Manufacturer_ID int FOREIGN KEY REFERENCES Manufacturer(Manufacturer_ID) NULL,
 Product_Name varchar(50),
 Product_Image varchar(100),
 Series varchar(20),
