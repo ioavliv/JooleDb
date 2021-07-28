c4f514db251d704553f259ae11364f2d5d004793
GWest96 <76221856+GWest96@users.noreply.github.com>
2021-07-28 14:35:21 -0400

    Update TypeFilter.sql

--- JooleDb/TypeFilter.sql
+++ JooleDb/TypeFilter.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[TypeFilter]
 (
-SC_ID int FOREIGN KEY REFERENCES SubCategory(SubCategory_ID) NOT NULL,
-Prop_ID int FOREIGN KEY REFERENCES Property(Property_ID) NOT NULL,
+SubCategory_ID int FOREIGN KEY REFERENCES SubCategory(SubCategory_ID) NOT NULL,
+Property_ID int FOREIGN KEY REFERENCES Property(Property_ID) NOT NULL,
 Type_Name varchar(50),
 Type_Option varchar(50)
 Primary Key (Property_ID,Subcategory_ID)
