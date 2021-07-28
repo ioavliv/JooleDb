e6af06f158b8f3dc4d180525101a1adb0bdf68ed
GWest96 <76221856+GWest96@users.noreply.github.com>
2021-07-28 14:34:53 -0400

    Update SubCategory.sql

--- JooleDb/SubCategory.sql
+++ JooleDb/SubCategory.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[SubCategory]
 (
 SubCategory_ID int NOT NULL PRIMARY KEY,
-C_ID int FOREIGN KEY REFERENCES Category(Category_ID) NULL,
+Category_ID int FOREIGN KEY REFERENCES Category(Category_ID) NULL,
 SubCategoryName varchar(30)
 );
