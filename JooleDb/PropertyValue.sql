6cfe5bec424ed7d484526bfc0177c36fc170ee05
GWest96 <76221856+GWest96@users.noreply.github.com>
2021-07-28 14:34:37 -0400

    Update PropertyValue.sql

--- JooleDb/PropertyValue.sql
+++ JooleDb/PropertyValue.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[PropertyValue]
 (
-Prod_ID int FOREIGN KEY REFERENCES Product(Product_ID) NOT NULL,
-Prop_ID int FOREIGN KEY REFERENCES Property(Property_ID) NOT NULL,
+Product_ID int FOREIGN KEY REFERENCES Product(Product_ID) NOT NULL,
+Property_ID int FOREIGN KEY REFERENCES Property(Property_ID) NOT NULL,
 Value varchar(50)
 PRIMARY KEY (Product_ID,Property_ID)
 );
