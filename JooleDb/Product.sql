5a95fca2ad4b47a38cdbc7112ba8547243838b05
ioavliv <ilivneh@nyit.edu>
2021-08-01 10:43:23 -0400

    Added Products and PropertyValues

--- JooleDb/Product.sql
+++ JooleDb/Product.sql
@@ -4,7 +4,7 @@ Product_ID int NOT NULL PRIMARY KEY,
 Subcategory_ID int FOREIGN KEY REFERENCES SubCategory(SubCategory_ID) NULL,
 Manufacturer_ID int FOREIGN KEY REFERENCES Manufacturer(Manufacturer_ID) NULL,
 Product_Name varchar(50),
-Product_Image varchar(100),
+Product_Image varchar(900),
 Series varchar(20),
 Model varchar(250)
 );
